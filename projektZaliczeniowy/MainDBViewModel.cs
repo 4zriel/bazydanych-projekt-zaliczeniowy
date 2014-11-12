c2c8b159ef3bfa4fa62e4bb76ef627dbcdbba49b
rafalklosek@gmail.com <rafalklosek@gmail.com@e6fbdcdb-dccd-3827-279a-2b63bbba6af3>
2014-11-12 06:14:57 +0000

    -dodanie brakujących plikow

---
+++ projektZaliczeniowy/MainDBViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektZaliczeniowy
+{
+	public class MainDBViewModel
+	{
+		ObservableCollection<DBStructureViewModel> _DBList = new ObservableCollection<DBStructureViewModel>();
+
+		public ObservableCollection<DBStructureViewModel> DBList
+		{
+			get
+			{
+				return _DBList;
+			}
+			set
+			{
+				_DBList = value;
+			}
+		}
+		public void addToList(DBStructureViewModel DBEntry)
+		{
+			_DBList.Add(DBEntry);
+		}
+		public void Clear()
+		{
+			_DBList.Clear();
+		}
+	}
+}
